6bac41b245e98f72a45a6b93ab5d2fc6d5605c2e
KingOfBadgers <iamkingofthebadgers@googlemail.com>
2026-07-14 11:21:42 +0100

    more frames

--- stage3/frames/frameRegistry.ts
+++ stage3/frames/frameRegistry.ts
@@ -246,6 +246,218 @@ export const frameRegistry: FrameDefinition[] = [
   },
 },
 
+ {
+  id:"four_square_panel_actor",
+  displayName:
+    "Four Square Panel — Actors",
+  src:
+    "/frames/four_square.png",
+  imageSource:
+    "actors",
+  maxAssets:
+    4,
+  canvas:{
+    width:1660,
+    height:1645,
+  },
+   imageSlots:[
+  {
+    id:"actor1",
+    x:122,
+    y:101,
+    width:666,
+    height:667,
+  },
+  {
+    id:"actor2",
+    x:868,
+    y:101,
+    width:666,
+    height:667,
+  },
+  {
+    id:"actor3",
+    x:122,
+    y:862,
+    width:666,
+    height:667,
+  },
+  {
+    id:"actor4",
+    x:868,
+    y:862,
+    width:666,
+    height:667,
+  },
+  ],
+
+  placement:{
+    mode:"width",
+    anchor:"center",
+    width:600,
+  },
+}
+,
+
+{
+  id:"four_square_panel_actor_v2",
+  displayName:
+    "Four Square Panel — Actors (2)",
+  src:
+    "/frames/foursquare.png",
+  imageSource:
+    "actors",
+  maxAssets:
+    4,
+  canvas:{
+    width:1923,
+    height:1894,
+  },
+   imageSlots:[
+  {
+    id:"actor1",
+    x:125,
+    y:114,
+    width:631,
+    height:642,
+  },
+  {
+    id:"actor2",
+    x:1148,
+    y:114,
+    width:631,
+    height:642,
+  },
+  {
+    id:"actor3",
+    x:129,
+    y:1090,
+    width:631,
+    height:642,
+  },
+  {
+    id:"actor4",
+    x:1180,
+    y:1088,
+    width:631,
+    height:642,
+  },
+  ],
+
+  placement:{
+    mode:"width",
+    anchor:"center",
+    width:600,
+  },
+}
+,
+
+{
+  id:"four_square_panel_backdrop_v2",
+  displayName:
+    "Four Square Panel — Backdrops (2)",
+  src:
+    "/frames/foursquare.png",
+  imageSource:
+    "backdrops",
+  maxAssets:
+    4,
+  canvas:{
+    width:1923,
+    height:1894,
+  },
+   imageSlots:[
+  {
+    id:"actor1",
+    x:125,
+    y:114,
+    width:631,
+    height:642,
+  },
+  {
+    id:"actor2",
+    x:1148,
+    y:114,
+    width:631,
+    height:642,
+  },
+  {
+    id:"actor3",
+    x:129,
+    y:1090,
+    width:631,
+    height:642,
+  },
+  {
+    id:"actor4",
+    x:1180,
+    y:1088,
+    width:631,
+    height:642,
+  },
+  ],
+
+  placement:{
+    mode:"width",
+    anchor:"center",
+    width:600,
+  },
+}
+,
+
+ {
+  id:"four_square_panel_backdrops",
+  displayName:
+    "Four Square Panel — Backdrop",
+  src:
+    "/frames/four_square.png",
+  imageSource:
+    "backdrops",
+  maxAssets:
+    4,
+  canvas:{
+    width:1660,
+    height:1645,
+  },
+   imageSlots:[
+  {
+    id:"actor1",
+    x:122,
+    y:101,
+    width:666,
+    height:667,
+  },
+  {
+    id:"actor2",
+    x:868,
+    y:101,
+    width:666,
+    height:667,
+  },
+  {
+    id:"actor3",
+    x:122,
+    y:862,
+    width:666,
+    height:667,
+  },
+  {
+    id:"actor4",
+    x:868,
+    y:862,
+    width:666,
+    height:667,
+  },
+  ],
+
+  placement:{
+    mode:"width",
+    anchor:"center",
+    width:600,
+  },
+}
+,
+
 {
   id: "polaroid_classic_actor",
 
@@ -501,7 +713,93 @@ export const frameRegistry: FrameDefinition[] = [
 
 },
 
+{
+  id: "movie_scatter_actor",
 
+  displayName:
+    "Movie Strip Scatter — Actors",
+  positionMode:"absolute",
+  src:
+    "/frames/singleframe.png",
+
+  imageSource:
+    "actors",
+
+  maxAssets:
+    4,
+
+  renderMode:
+    "perSlot",
+
+  canvas:{
+    width:1660,
+    height:1645,
+  },
+
+  imageSlots:[
+
+{
+ id:"card1",
+ x:80,
+ y:520,
+ width:1600,
+ height:1200,
+ rotation:-22,
+ imageOffsetX:0,
+ imageOffsetY:30,
+ imageScale:1,
+},
+
+{
+ id:"card2",
+ x:280,
+ y:330,
+ width:1600,
+ height:1200,
+ rotation:12,
+ imageOffsetX:0,
+ imageOffsetY:32,
+ imageScale:1,
+},
+
+{
+ id:"card3",
+ x:500,
+ y:500,
+ width:1600,
+ height:1200,
+ rotation:-8,
+ imageOffsetX:0,
+ imageOffsetY:25,
+ imageScale:1,
+},
+
+{
+ id:"card4",
+ x:750,
+ y:380,
+ width:1600,
+ height:1200,
+ rotation:18,
+ imageOffsetX:0,
+ imageOffsetY:30,
+ imageScale:1,
+},
+
+],
+
+  placement:{
+
+    mode:"contain",
+    anchor:"center",
+
+    width:200,
+
+    transformOrigin:"center center",
+
+  },
+
+},
 
 {
   id: "polaroid_fan_3_backdrop",
@@ -566,6 +864,194 @@ export const frameRegistry: FrameDefinition[] = [
     transformOrigin:"left bottom",
 
 },
+},
+
+{id: "three_horiz_panel_backdrop",
+
+  displayName:
+    "Wooden Panel (3) — Backdrop",
+
+  src:
+    "/frames/wooden3.png",
+
+  imageSource:
+    "backdrops",
+
+  maxAssets:
+    3,
+
+  canvas:{
+    width:1918,
+    height:645,
+  },
+
+  imageSlots:[
+  {
+    id:"backdrop1",
+    x:110,
+    y:107,
+    width:437,
+    height:437,
+  },
+  {
+    id:"backdrop2",
+    x:745,
+    y:107,
+    width:437,
+    height:437,
+  },
+  {
+    id:"backdrop3",
+    x:1381,
+    y:107,
+    width:437,
+    height:437,
+  },
+],
+
+  placement:{
+    mode:"contain",
+    anchor:"top",
+    width:1000,
+  },
+},
+
+{id: "three_horiz_panel_actorsp",
+
+  displayName:
+    "Wooden Panel (3) — Actors",
+
+  src:
+    "/frames/wooden3.png",
+
+  imageSource:
+    "actors",
+
+  maxAssets:
+    3,
+
+  canvas:{
+    width:1918,
+    height:645,
+  },
+
+  imageSlots:[
+  {
+    id:"backdrop1",
+    x:110,
+    y:107,
+    width:437,
+    height:437,
+  },
+  {
+    id:"backdrop2",
+    x:745,
+    y:107,
+    width:437,
+    height:437,
+  },
+  {
+    id:"backdrop3",
+    x:1381,
+    y:107,
+    width:437,
+    height:437,
+  },
+],
+
+  placement:{
+    mode:"width",
+    anchor:"top",
+    width:1000,
+  },
+},
+
+{
+  id: "movie_scatter_4_backdrops",
+
+  displayName:
+    "Movie Scatter — Backdrop",
+  positionMode:"absolute",
+  src:
+    "/frames/singleframe.png",
+
+  imageSource:
+    "backdrops",
+
+  maxAssets:
+    4,
+
+  renderMode:
+    "perSlot",
+
+  canvas:{
+    width:1600,
+    height:1645,
+  },
+
+  imageSlots:[
+
+{
+ id:"card1",
+ x:50,
+ y:560,
+ width:1560,
+ height:1190,
+ rotation:-22,
+ imageOffsetX:0,
+ imageOffsetY:45,
+ imageScale:1,
+},
+
+{
+ id:"card2",
+ x:230,
+ y:260,
+ width:1380,
+ height:1390,
+ rotation:12,
+ imageOffsetX:30,
+ imageOffsetY:30,
+ imageScale:1.03,
+},
+
+{
+ id:"card3",
+ x:500,
+ y:600,
+ width:1500,
+ height:1190,
+ rotation:-8,
+ imageOffsetX:5,
+ imageOffsetY:40,
+ imageScale:1.02,
+},
+
+{
+ id:"card4",
+ x:650,
+ y:250,
+ width:1500,
+ height:1100,
+ rotation:18,
+ imageOffsetX:5,
+ imageOffsetY:45,
+ imageScale:1.04,
+},
+
+],
+
+  placement:{
+
+    mode:"width",
+    anchor:"center",
+
+    width:300,
+
+    transformOrigin:"center center",
+
+  },
+
 },
 
 
